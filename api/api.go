15717a76bcaa043220da412b4fc432598b4dbd64
chlovec <alfred.okoronkwo@gmail.com>
2025-01-22 08:38:01 -0800

    first commit

---
+++ api/api.go
@@ -0,0 +1,86 @@
+package api
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	server       *http.Server
+	apiRouter    *mux.Router
+	apiSubrouter *mux.Router
+	logger       *log.Logger
+}
+
+func NewAPIServer(addr string, pathPrefix string, logger *log.Logger) *APIServer {
+	router := mux.NewRouter()
+	subrouter := router
+
+	if pathPrefix != "" {
+		subrouter = router.PathPrefix(pathPrefix).Subrouter()
+	}
+
+	server := &http.Server{
+		Addr:    addr,
+		Handler: router,
+	}
+
+	return &APIServer{
+		apiRouter:    router,
+		apiSubrouter: subrouter,
+		server:       server,
+		logger:       logger,
+	}
+}
+
+func (s *APIServer) RegisterRoute(path string, handler func(http.ResponseWriter, *http.Request, *log.Logger)) {
+	if path == "" {
+		s.logger.Println("Cannot register a route with an empty path")
+		return
+	}
+	if handler == nil {
+		s.logger.Println("Cannot register a route with a nil handler")
+		return
+	}
+
+	// Wrap the handler to pass the logger
+	wrappedHandler := func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, s.logger)
+	}
+
+	s.apiSubrouter.HandleFunc(path, wrappedHandler)
+	s.logger.Printf("Route registered: %s", path)
+}
+
+func (s *APIServer) Start() error {
+	log.Printf("Starting server on %s...", s.server.Addr)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	errChan := make(chan error, 1)
+
+	go func() {
+		errChan <- s.server.ListenAndServe()
+	}()
+
+	select {
+	case <-stop:
+		log.Println("Shutting down gracefully...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := s.server.Shutdown(ctx); err != nil {
+			return err
+		}
+		log.Println("Server stopped gracefully.")
+		return nil
+	case err := <-errChan:
+		return err
+	}
+}
